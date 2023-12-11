5d93ee9d655a44074de545c946849b7657fa7a5f
rsssss12 <104445931+rsssss12@users.noreply.github.com>
2023-12-11 17:41:28 +0100

    Update ticket.sol
    Added returnTicket function (only during sale) + buyForOthers function

--- ticket.sol
+++ ticket.sol
@@ -112,4 +112,47 @@ contract EventTicket {
 
         return tickets.length - 1;
     }
+
+    // Buy ticket for another person
+    function buyTicketForOtherPerson(uint256 sectionId, address otherPerson) external payable returns (uint256) {
+        require(saleOpen == true, "Sale must be open");
+        require(sectionId < sections.length, "Section ID must be valid");
+        require(
+            sections[sectionId].sold < sections[sectionId].num_tickets,
+            "Section sold out"
+        );
+        require(
+            msg.value == sections[sectionId].price,
+            "Full price of ticket must be paid"
+        );
+
+        Ticket memory ticket;
+        ticket.sectionId = sectionId;
+        ticket.owner = otherPerson;
+        tickets.push(ticket);
+        sections[sectionId].num_tickets--;
+        sections[sectionId].sold++;
+
+        return tickets.length - 1;
+    }
+
+    // Return an own ticket
+     function returnTicket(uint256 id) public {
+        require(saleOpen == true, "Sale must be open");
+        require(id < tickets.length, "Invalid ticket ID");
+        require(tickets[id].owner == msg.sender, "Not owner of ticket");
+        require(tickets[id].used == false, "Ticket already used");
+
+        // Remove ticket from tickets array
+        for (uint i = id; i < tickets.length - 1; i++) {
+            tickets[i] = tickets[i+1];
+        }
+        tickets.pop();
+
+        sections[tickets[id].sectionId].num_tickets++;
+        sections[tickets[id].sectionId].sold--;
+
+        // Refund the ticket price to the ticket owner
+        payable(msg.sender).transfer(sections[tickets[id].sectionId].price);
+    }
 }
