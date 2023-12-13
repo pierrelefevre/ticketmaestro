4e9d036136d8135f1a8f3c6c807a99117c546927
rsssss12 <104445931+rsssss12@users.noreply.github.com>
2023-12-13 05:58:23 +0100

    Update EventTicket_test2.sol

--- test/EventTicket_test2.sol
+++ test/EventTicket_test2.sol
@@ -3,12 +3,12 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 // This import is automatically injected by Remix
-import "https://raw.githubusercontent.com/pierrelefevre/ticketmaestro/main/test/remix_tests.sol";
+import "./test/remix_tests.sol";
 
 // This import is required to use custom transaction context
 // Although it may fail compilation in 'Solidity Compiler' plugin
 // But it will work fine in 'Solidity Unit Testing' plugin
-import "https://raw.githubusercontent.com/pierrelefevre/ticketmaestro/main/contracts/EventTicket.sol";
+import "./contracts/EventTicket.sol";
 
 // File name has to end with '_test.sol', this file can contain more than one testSuite contracts
 contract testSuite {
